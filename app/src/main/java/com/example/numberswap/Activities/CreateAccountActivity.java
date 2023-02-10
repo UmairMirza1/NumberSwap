83d10109da07ef3cff5ddf855a8a7ae9bd7989f6
Adil MIr <l191230@lhr.nu.edu.pk>
2023-02-10 21:20:49 +0500

    12th Commit

--- app/src/main/java/com/example/numberswap/Activities/CreateAccountActivity.java
+++ app/src/main/java/com/example/numberswap/Activities/CreateAccountActivity.java
@@ -5,19 +5,29 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.numberswap.DB.BusinessCardDB;
+import com.example.numberswap.Interface.IBusinessCardDAO;
+import com.example.numberswap.JavaClasses.BusinessCard;
 import com.example.numberswap.R;
 import com.hbb20.CountryCodePicker;
 
@@ -27,14 +37,18 @@ import java.util.Calendar;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
-    EditText email, phone, name,dob;
+    EditText email, phone, name;
+    TextView dob;
     ImageView displayPicture;
     CountryCodePicker countryCodePicker;
     String selectedCode;
+    Button create,create1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+
         name = findViewById(R.id.editTextTextPersonName);
         email = findViewById(R.id.editTextTextEmailAddress2);
         phone = findViewById(R.id.personPhone);
@@ -42,6 +56,8 @@ public class CreateAccountActivity extends AppCompatActivity {
         displayPicture = findViewById(R.id.displayPicture);
         countryCodePicker = findViewById(R.id.ccp);
         selectedCode = countryCodePicker.getSelectedCountryCode();
+        create = findViewById(R.id.create);
+        create1 = findViewById(R.id.create1);
 
         displayPicture.setOnClickListener(v->
         {
@@ -50,49 +66,44 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         dob.setOnClickListener(v->
         {
-            final Calendar c = Calendar.getInstance();
-
-            // on below line we are getting
-            // our day, month and year.
-            int years = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // on below line we are creating a variable for date picker dialog.
-            DatePickerDialog datePickerDialog = new DatePickerDialog(CreateAccountActivity.this, new DatePickerDialog.OnDateSetListener() {
-                @Override
-                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                            // on below line we are setting date to our text view.
-                    if(years<year)
-                    {
-                        Toast.makeText(CreateAccountActivity.this, "Year Can't be Greater than Current Year!", Toast.LENGTH_SHORT).show();
-                    }
-                    else if(month<(monthOfYear) && year==years)
-                    {
-                        Toast.makeText(CreateAccountActivity.this, "Month can't be Greater than Current Month!", Toast.LENGTH_SHORT).show();
-                    }
-                    else if(day<dayOfMonth && (month == monthOfYear) && year == years)
-                    {
-                        Toast.makeText(CreateAccountActivity.this, "Date Can't be Greater Than Today's Date", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        dob.setText((monthOfYear + 1) + "-" + dayOfMonth + "-" + year);
-                    }
-
-                        }
-                    },years, month, day);
-            datePickerDialog.show();
+         pickDate();
         });
-//        setViews();
-//        setListeners();
-    }
 
-    private void setListeners() {
+        create.setOnClickListener(v->
+        {
+            if(TextUtils.isEmpty(name.getText().toString())) {
+                Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else if(TextUtils.isEmpty(phone.getText().toString())) {
+                Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else if(TextUtils.isEmpty(email.getText().toString())) {
+                Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else if(displayPicture.getDrawable() == null)
+            {
+                Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+              create.setVisibility(View.GONE);
+              create1.setVisibility(View.VISIBLE);
+            }
 
-    }
+        });
+        create1.setOnClickListener(v->
+        {
+            BusinessCard businessCard = new BusinessCard(email.getText().toString(),
+                    phone.getText().toString(),
+                    dob.getText().toString());
+            IBusinessCardDAO db = new BusinessCardDB(this);
+            businessCard.setDao(db);
+            Bitmap bm=((BitmapDrawable)displayPicture.getDrawable()).getBitmap();
+            businessCard.setImage(bm);
+            businessCard.setFullName(name.getText().toString());
+            businessCard.save();
 
-    private void setViews() {
+        });
     }
     private void imageChooser()
     {
@@ -122,4 +133,54 @@ public class CreateAccountActivity extends AppCompatActivity {
                     }
                 }
             });
+    private void pickDate()
+    {
+        final Calendar c = Calendar.getInstance();
+        int years = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // on below line we are creating a variable for date picker dialog.
+        DatePickerDialog datePickerDialog = new DatePickerDialog(CreateAccountActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // on below line we are setting date to our text view.
+                if(years<year)
+                {
+                    Toast.makeText(CreateAccountActivity.this, "Year Can't be Greater than Current Year!", Toast.LENGTH_SHORT).show();
+                }
+                else if(month<(monthOfYear) && year==years)
+                {
+                    Toast.makeText(CreateAccountActivity.this, "Month can't be Greater than Current Month!", Toast.LENGTH_SHORT).show();
+                }
+                else if(day<dayOfMonth && (month == monthOfYear) && year == years)
+                {
+                    Toast.makeText(CreateAccountActivity.this, "Date Can't be Greater Than Today's Date", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    dob.setText((monthOfYear + 1) + "-" + dayOfMonth + "-" + year);
+                }
+
+            }
+        },years, month, day);
+        datePickerDialog.show();
+    }
+    private boolean isFilled() {
+            if(TextUtils.isEmpty(name.getText().toString())) {
+            return false;
+        }
+            if(TextUtils.isEmpty(phone.getText().toString())) {
+            return false;
+        }
+            if(TextUtils.isEmpty(email.getText().toString())) {
+            return false;
+        }
+        if(displayPicture.getDrawable() == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
